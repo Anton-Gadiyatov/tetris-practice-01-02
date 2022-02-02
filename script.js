2fbfbed4c0bac3dfbcef734a60cdafbfcd4eff21
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-02-02 23:29:50 +0300

    feat: add moveTetroDown function

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const playfield = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -25,10 +25,30 @@ const playfield = [
 
 let mainInnerHTML = '';
 
-playfield.forEach((row) => {
-  row.forEach((cell) => {
-    mainInnerHTML += `<div class='cell'></div>`;
+const draw = () => {
+  playfield.forEach((row, rowIndex) => {
+    row.forEach((cell, cellIndex) => {
+      if (cell === 1) {
+        mainInnerHTML += `<div class='cell moving-cell'></div>`;
+      } else {
+        mainInnerHTML += `<div class='cell'></div>`;
+      }
+    });
   });
-});
 
-main.innerHTML = mainInnerHTML;
+  main.innerHTML = mainInnerHTML;
+};
+
+const moveTetroDown = () => {
+  for (let y = playfield.length - 1; y >= 0; y--) {
+    for (let x = playfield[y].length - 1; x >= 0; x--) {
+      if (playfield[y][x] === 1) {
+        playfield[y][x] = 0;
+        playfield[y + 1][x] = 1;
+      }
+    }
+  }
+};
+
+draw();
+moveTetroDown();
