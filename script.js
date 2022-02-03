9c9e8c6505db7367d0aabcb965a57fdb8c19c19a
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-02-03 23:07:15 +0300

    feat: add canTetoMoveDown function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const main = document.querySelector('.main');
 
 const playfield = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
@@ -23,11 +23,12 @@ const playfield = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-let mainInnerHTML = '';
+let gameSpeed = 400;
 
 const draw = () => {
-  playfield.forEach((row, rowIndex) => {
-    row.forEach((cell, cellIndex) => {
+  let mainInnerHTML = '';
+  playfield.forEach((row) => {
+    row.forEach((cell) => {
       if (cell === 1) {
         mainInnerHTML += `<div class='cell moving-cell'></div>`;
       } else {
@@ -39,16 +40,38 @@ const draw = () => {
   main.innerHTML = mainInnerHTML;
 };
 
-const moveTetroDown = () => {
-  for (let y = playfield.length - 1; y >= 0; y--) {
-    for (let x = playfield[y].length - 1; x >= 0; x--) {
+const canTetroMoveDown = () => {
+  for (let y = 0; y < playfield.length; y++) {
+    for (let x = 0; x < playfield[y].length; x++) {
       if (playfield[y][x] === 1) {
-        playfield[y][x] = 0;
-        playfield[y + 1][x] = 1;
+        if (y === playfield.length - 1) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+};
+
+const moveTetroDown = () => {
+  if (canTetroMoveDown()) {
+    for (let y = playfield.length - 1; y >= 0; y--) {
+      for (let x = playfield[y].length - 1; x >= 0; x--) {
+        if (playfield[y][x] === 1) {
+          playfield[y][x] = 0;
+          playfield[y + 1][x] = 1;
+        }
       }
     }
   }
 };
 
 draw();
-moveTetroDown();
+
+const startGame = () => {
+  moveTetroDown();
+  draw();
+  setTimeout(startGame, gameSpeed);
+};
+
+setTimeout(startGame, gameSpeed);
